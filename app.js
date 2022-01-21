1b8370c9c19b0cd33d2f69b2df9f0a72075beb26
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2022-01-21 14:52:23 -0800

    finish game

--- app.js
+++ app.js
@@ -4,32 +4,46 @@ const squares = document.querySelectorAll(".square");
 const mole = document.querySelector(".mole");
 const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
+const resetBtn = document.querySelector(".reset");
 
-let result = 0;
+let result;
 let hitPosition;
-let currentTime = 60;
+let currentTime;
 let timerId;
+let canPlay = true;
 const moveMoleTimer = 1;
 
-function randomSquare() {
+function removeMole() {
   squares.forEach((square) => {
     square.classList.remove("mole");
   });
+}
+
+function randomSquare() {
+  removeMole();
 
   let randomSquare = squares[Math.floor(Math.random() * squares.length)];
   randomSquare.classList.add("mole");
 
   hitPosition = randomSquare.id;
 }
-randomSquare()
+
+function flashSquare(sq) {
+  sq.style.backgroundColor = "red";
+  setInterval(() => {
+    sq.style.backgroundColor = "transparent";
+  }, 100);
+}
+
 
 squares.forEach((square) => {
-  square.addEventListener("click", () => {
+  square.addEventListener("click", (e) => {
     if (square.id == hitPosition) {
+      flashSquare(e.target)
       result++;
       score.textContent = result;
       hitPosition = null;
-      randomSquare()
+      randomSquare();
     }
   });
 });
@@ -38,17 +52,38 @@ function moveMole() {
   timerId = setInterval(randomSquare, moveMoleTimer * 1000);
 }
 
-moveMole();
-
 function countDown() {
   currentTime--;
   timeLeft.textContent = currentTime;
 
-  if (currentTime == 0) {
+  if (currentTime <= 0) {
     clearInterval(countDownTimerId);
     clearInterval(timerId);
     alert("GAME OVER! Your final score is " + result);
+    canPlay = true;
+    removeMole();
   }
 }
 
-let countDownTimerId = setInterval(countDown, 1000);
+let countDownTimerId;
+
+function init() {
+  if (!canPlay) return;
+  result = 0;
+  hitPosition;
+  currentTime = 60;
+  timerId;
+  canPlay = false;
+
+  score.textContent = result;
+  timeLeft.textContent = currentTime;
+
+  randomSquare();
+  moveMole();
+
+  countDownTimerId = setInterval(countDown, 1000);
+}
+
+init();
+
+resetBtn.addEventListener("click", init);
